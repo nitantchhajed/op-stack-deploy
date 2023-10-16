2a1c53fc60ca6900ea9de3b423c1e37d308869dc
L2nitant <n.chhajed@race.community>
2023-10-16 13:10:18 +0530

    heatbeat setup

---
+++ heartbeat/heartbeat-identifier.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#Check for health status
+
+HEALTH_STATUS=$(curl -s http://172.22.68.224/health | jq -r '.healthy')
+
+#check for current date and time log logs
+
+CURRENT_DATE_TIME=$(date)
+
+#check if status is true
+
+if [ "$HEALTH_STATUS" == "true" ]; then
+    echo "Service is healthy. No action needed. -- $CURRENT_DATE_TIME"
+else
+    echo "Service is not healthy, converting this node to Sequencer node -- $CURRENT_DATE_TIME"
+    docker start <container ID>
+fi
